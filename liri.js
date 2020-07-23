d4ac416b576d12f5776d35cdc7d2d4a8c93745c0
Raymond _ <raymondt1234@gmail.com>
2020-07-22 19:23:44 -0700

    do-what-it-says option works. user can enter the command do-what-it-says and the app will read the command and artist or movie from random.txt file

--- liri.js
+++ liri.js
@@ -8,6 +8,8 @@ const moment = require("moment");
 
 const keys = require("./keys.js");
 
+const fs = require("fs");
+
 const spotify = new Spotify(keys.spotify);
 
 function trackSearch(track) {
@@ -57,6 +59,9 @@ function concertThis(band) {
             } else {
                 console.log("Nothing was found for that Artist/Band.");
             }
+        })
+        .catch(function (error) {
+            console.log(error);
         });
 }
 function movieThis(movie) {
@@ -77,37 +82,60 @@ function movieThis(movie) {
             console.log(`Language(s):               ${movieInfo.Language}`);
             console.log(`Actors/Actresses:          ${movieInfo.Actors}`);
             console.log(`Plot:                      ${movieInfo.Plot}`);
+        })
+        .catch(function (error) {
+            console.log(error);
         });
+}
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        console.log(data);
+
+        let dataArr = data.split(",");
+
+        let doWhat = dataArr[0];
+        let search = dataArr[1];
+
+        whatOption(doWhat, search);
+    });
+}
+function whatOption (option, searchFor) {
+
+    switch (option) {
+        case "concert-this":
+            concertThis(searchFor);
+            break;
+
+        case "spotify-this-song":
+            trackSearch(searchFor);
+            break;
+
+        case "movie-this":
+            movieThis(searchFor);
+            break;
+
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+
+        default:
+            console.log("That isn't an option...");
+            break;
+    }
 }
 
 let option = process.argv[2];
 let searchFor = "";
 
-for (let i = 3; i < process.argv.length; i++) {
-    if (i > 3) {
-        searchFor += " ";
+if (process.argv.length > 2) {
+    for (let i = 3; i < process.argv.length; i++) {
+        if (i > 3) {
+            searchFor += " ";
+        }
+        searchFor += process.argv[i];
     }
-    searchFor += process.argv[i];
 }
+whatOption(option, searchFor);
 
-switch (option) {
-    case "concert-this":
-        concertThis(searchFor);
-        break;
 
-    case "spotify-this-song":
-        trackSearch(searchFor);
-        break;
 
-    case "movie-this":
-        movieThis(searchFor);
-        break;
-
-    case "do-what-it-says":
-        console.log("do-what-it-says doesn't work yet...");
-        break;
-
-    default:
-        console.log("That isn't an option...");
-        break;
-}
